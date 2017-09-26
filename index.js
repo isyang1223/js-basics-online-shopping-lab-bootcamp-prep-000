4020591fa6e0f83758a746f1f0796cea87ff5e34
isyang1223 <isyang1223@gmail.com>
2017-09-26 23:01:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.");
     return false;
   }
-  console.log(`Your total cost is $${total()}, which will be charged to the car ${cardNumber}`);
-  cart=[];
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  cart = [];
   // write your code here
 }
