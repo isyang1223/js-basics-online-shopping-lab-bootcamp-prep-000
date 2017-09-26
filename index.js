b52f48e347afab743ab69663c0693f5cc99146e4
isyang1223 <isyang1223@gmail.com>
2017-09-26 22:17:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,13 +12,17 @@ function setCart(c) {
 function addToCart(item) {
   var price = Math.floor(Math.random() * 100) + 1;
   cart.push({[item]: price});
-  console.log(`${item} has been added to your cart.`)
-  return cart
- // write your code here
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    console.log(`Your Shopping cart is empty.`)
+  }
+  for(i = 0; i < cart.length; i++){
+    
+  }
 }
 
 function total() {
