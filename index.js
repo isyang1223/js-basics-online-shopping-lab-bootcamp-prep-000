5d3f4aa35fb9ef7ab51a8366ebd0acea554bbd81
isyang1223 <isyang1223@gmail.com>
2017-09-26 22:34:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,27 @@ function viewCart() {
 var l = cart.length;
 if(!l){
   return console.log('your shopping cart is empty.');
-}}
+}
+var itemsAndPrices = [];
+
+for (var = i; i < l; i++){
+  var itemsAndPrices = cart[i];
+  var item = Object.keys(itemsAndPrices)[0];
+  var price = itemsAndPrices[item];
+  itemsAndPrices.push(`${item} at \$${price}`);
+}
+switch (itemsAndPrices.length) {
+  case 1:
+    break;
+  case 2:
+    itemsAndPrices = itemsAndPrices.join(' and ');
+    break;
+  default:
+    itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+    itemsAndPrices = itemsAndPrices.join(', ');
+}
+console.log(`In your cart, you have ${itemsAndPrices}.`)
+}
 
 function total() {
   // write your code here
