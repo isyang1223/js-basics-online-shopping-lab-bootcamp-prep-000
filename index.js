258b250a7a85eee95118ba9e8f1ea1f107770b28
isyang1223 <isyang1223@gmail.com>
2017-09-26 22:52:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function total() {
 
 function removeFromCart(item) {
   var itemInCart = false;
-  for (var i = 0,l = cart.length; i < cart.length; i++) {
+  for (var i = 0,l = cart.length; i < l; i++) {
     if (cart[i].hasOwnProperty(item)) {
       itemInCart = true;
       cart = cart.slice(0,i).concat(cart.slice(i + 1));
